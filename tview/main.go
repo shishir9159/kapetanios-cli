91e9fe7c01bf26505c0a48addc3e98e99df263b6
Nahid Hasan <37985254+shishir9159@users.noreply.github.com>
2025-01-18 15:06:30 +0600

    updated tview

--- tview/main.go
+++ tview/main.go
@@ -1,1 +1,40 @@
 package main
+
+import (
+  	"flag"
+	"fmt"
+
+	"github.com/shishir9159/kapetanios-cli/pkg/parser"
+	"github.com/shishir9159/kapetanios-cli/pkg/ui"
+	"github.com/rivo/tview"
+)
+
+// unified pkg parser and ui is work in progress
+
+var (
+	path = flag.String("path", "./", "specified start path. default is ./")
+	help = flag.Bool("help", false, "displayed help message for kapetanios")
+)
+
+func main() {
+	flag.Parse()
+
+	if *help {
+		fmt.Printf(`kapetanios-cli is a kubernetes admistration tool.
+
+Usage:
+	kapetanios-cli [options]
+
+The options are:
+	--path=$PATH specified start path. default is ./
+	--help       displayed help message for itree
+`,
+		)
+		return
+	}
+
+box := tview.NewBox().SetBorder(true).SetTitle("Hello, world!")
+	if err := tview.NewApplication().SetRoot(box, true).Run(); err != nil {
+		panic(err)
+	}
+}
